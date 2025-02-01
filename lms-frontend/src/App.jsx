35ae086828a98731b7cae86501174b58c35be5ae
Niharika Thakur <thakurniharika2003@gmail.com>
2025-02-01 23:21:22 +0530

    auth frontend

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import AuthPage from './pages/auth'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+       <Route path='/auth' element={<AuthPage/>} />
+    </Routes>
   )
 }
 
--- lms-frontend/src/main.jsx
+++ lms-frontend/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import AppRoutes from "./routes";
-import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import AppRoutes from "./routes";
+import App from "./App.jsx";
+import AuthProvider from "./context/auth-context";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AppRoutes />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+);
